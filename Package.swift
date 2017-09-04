10be0a0b8723b0532073570253f5e0af2dd1b55d
张戈弋 <zhanggeyi@meituan.com>
2017-09-04 15:01:40 +0800

    添加Package.swift文件

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "PerfectDB",
+    targets: [],
+    dependencies: [
+        //HTTPServer
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+        
+        //Request请求日志过滤器
+        .Package(url: "https://github.com/dabfleming/Perfect-RequestLogger.git",
+                 majorVersion: 0),
+        
+        //将日志写入指定文件
+//        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
+//        .Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 1),
+        
+        //MySql数据库依赖包
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git",
+                 majorVersion: 2, minor: 0)
+    ]
+
+)
